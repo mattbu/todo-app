36e3321f2fe7fea9cf400fe9c1accfeac844cae2
mattbu <chillysaucing@gmail.com>
2022-07-15 01:00:21 +0900

    시간 띄우기, 인풋 스타일링

---
+++ components/Clock.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/components/Clock.module.scss";
+
+const Clock = () => {
+  const [HH, setHH] = useState("");
+  const [MM, setMM] = useState("");
+  const [SS, setSS] = useState("");
+
+  useEffect(() => {
+    setInterval(() => {
+      const today = new Date();
+      const hours = String(today.getHours()).padStart(2, "0");
+      const minutes = String(today.getMinutes()).padStart(2, "0");
+      const seconds = String(today.getSeconds()).padStart(2, "0");
+      setHH(hours);
+      setMM(minutes);
+      setSS(seconds);
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      <h1 className={styles.clock}>{`${HH}:${MM}:${SS}`}</h1>
+    </div>
+  );
+};
+
+export default Clock;
