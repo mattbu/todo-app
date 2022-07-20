e6f28c87e35dae5ac9c390fd0de2bff83aa513d2
mattbu <chillysaucing@gmail.com>
2022-07-20 20:42:43 +0900

    fix

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -1,16 +1,15 @@
 import { useState, ChangeEvent, FormEvent, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { UserState } from "../slices/userSlice";
 import { RootState } from "../store";
 import styles from "../styles/components/Greeting.module.scss";
 import { setUserName } from "../slices/userSlice";
-import { useDispatch } from "react-redux";
 import { persistor } from "../store";
 
-import CustomInput from './CustomInput'
+import CustomInput from "./CustomInput";
 
 export default function MainInput() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const [name, setName] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -19,35 +18,44 @@ export default function MainInput() {
     setName(e.target.value);
   };
 
-  const currentUser = useSelector<RootState, UserState>(state => state.user)
+  const currentUser = useSelector<RootState, UserState>((state) => state.user);
 
   const submitName = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(setUserName({ name }))
+    dispatch(setUserName({ name }));
     setIsSubmitted(true);
   };
 
   const exit = async () => {
-    setName('')
+    setName("");
     await setTimeout(() => {
-      persistor.purge()
-    }, 500)
+      persistor.purge();
+    }, 500);
     await setTimeout(() => {
       setIsSubmitted(false);
-    }, 1000)
-  }
+    }, 1000);
+  };
   useEffect(() => {
-    return () => clearTimeout()
-  })
+    return () => clearTimeout();
+  });
   return (
     <div className={styles.container}>
       <div>
         <h1
-          className={currentUser.name ? `${styles.greeting} ${styles['show']}` : `${styles.greeting} ${styles['hide']}`}
+          className={
+            currentUser.name
+              ? `${styles.greeting} ${styles["show"]}`
+              : `${styles.greeting} ${styles["hide"]}`
+          }
         >
-          {currentUser.name ? `안녕하세요,` : '안녕히 계세요.'} {currentUser.name}
+          {currentUser.name ? `안녕하세요,` : "안녕히 계세요."}{" "}
+          {currentUser.name}
         </h1>
-        {currentUser.name && <button className={styles.exitBtn} onClick={exit}>나가기</button>}
+        {currentUser.name && (
+          <button className={styles.exitBtn} onClick={exit}>
+            나가기
+          </button>
+        )}
       </div>
       {!currentUser.name && !isSubmitted ? (
         <div>
@@ -58,8 +66,7 @@ export default function MainInput() {
             placeholder="당신은 누구십니까?"
           />
         </div>
-      ) : (null
-      )}
+      ) : null}
     </div>
   );
-};
+}
